976912804c7ec5f94bf1a48e681cd1918c18717b
Andre Lucyo <andre.lucyo2@gmail.com>
2020-08-10 01:06:34 -0300

    Criado classe Produto

--- src/model/entidades/Produto.java
+++ src/model/entidades/Produto.java
@@ -6,6 +6,8 @@
 package model.entidades;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Objects;
 
 /**
  *
@@ -13,12 +15,155 @@ import java.io.Serializable;
  */
 public class Produto implements Serializable
 {
-    private int id;
-    private int tipoproduto_id;
+    private int id;    
+    
     private String nome;
     private String descricao;
     private Double preco_unitario;
     private String unidade;
     private boolean registro_ativo;
+    
+    private TipoProduto tipoproduto;
+    
+    private ArrayList<ProdutorProduto> ProdutorProdutos = new ArrayList<ProdutorProduto>();
+
+    public Produto()
+    {
+    }
+
+    public Produto(int id, String nome, String descricao, Double preco_unitario, String unidade, boolean registro_ativo, TipoProduto tipoproduto)
+    {
+	this.id = id;
+	this.nome = nome;
+	this.descricao = descricao;
+	this.preco_unitario = preco_unitario;
+	this.unidade = unidade;
+	this.registro_ativo = registro_ativo;
+	this.tipoproduto = tipoproduto;
+    }
+
+    public int getId()
+    {
+	return id;
+    }
+
+    public void setId(int id)
+    {
+	this.id = id;
+    }
+
+    public String getNome()
+    {
+	return nome;
+    }
+
+    public void setNome(String nome)
+    {
+	this.nome = nome;
+    }
+
+    public String getDescricao()
+    {
+	return descricao;
+    }
+
+    public void setDescricao(String descricao)
+    {
+	this.descricao = descricao;
+    }
+
+    public Double getPreco_unitario()
+    {
+	return preco_unitario;
+    }
+
+    public void setPreco_unitario(Double preco_unitario)
+    {
+	this.preco_unitario = preco_unitario;
+    }
+
+    public String getUnidade()
+    {
+	return unidade;
+    }
+
+    public void setUnidade(String unidade)
+    {
+	this.unidade = unidade;
+    }
+
+    public boolean isRegistro_ativo()
+    {
+	return registro_ativo;
+    }
+
+    public void setRegistro_ativo(boolean registro_ativo)
+    {
+	this.registro_ativo = registro_ativo;
+    }
+
+    public TipoProduto getTipoproduto()
+    {
+	return tipoproduto;
+    }
+
+    public void setTipoproduto(TipoProduto tipoproduto)
+    {
+	this.tipoproduto = tipoproduto;
+    }
+
+    public ArrayList<ProdutorProduto> getProdutorProdutos()
+    {
+	return ProdutorProdutos;
+    }
+
+    public void setProdutorProdutos(ArrayList<ProdutorProduto> ProdutorProdutos)
+    {
+	this.ProdutorProdutos = ProdutorProdutos;
+    }
+
+    @Override
+    public int hashCode()
+    {
+	int hash = 7;
+	hash = 83 * hash + Objects.hashCode(this.nome);
+	hash = 83 * hash + Objects.hashCode(this.tipoproduto);
+	return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+	if (this == obj)
+	{
+	    return true;
+	}
+	if (obj == null)
+	{
+	    return false;
+	}
+	if (getClass() != obj.getClass())
+	{
+	    return false;
+	}
+	final Produto other = (Produto) obj;
+	if (!Objects.equals(this.nome, other.nome))
+	{
+	    return false;
+	}
+	if (!Objects.equals(this.tipoproduto, other.tipoproduto))
+	{
+	    return false;
+	}
+	return true;
+    }
+
+    @Override
+    public String toString()
+    {
+	return "Produto{" + "id=" + id + ", nome=" + nome + ", descricao=" + descricao + ", preco_unitario=" + preco_unitario + ", unidade=" + unidade + ", registro_ativo=" + registro_ativo + ", tipoproduto=" + tipoproduto + ", ProdutorProdutos=" + ProdutorProdutos + '}';
+    }
+    
+    
     
 }
