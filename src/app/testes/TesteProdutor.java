8fa9c2c98007191b6705e326747570f84ee25145
Andre Lucyo <andre.lucyo2@gmail.com>
2020-08-10 01:17:04 -0300

    iniciado teste de produtor console

---
+++ src/app/testes/TesteProdutor.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app.testes;
+
+/**
+ *
+ * @author Andre
+ */
+public class TesteProdutor
+{
+
+    public static void main(String[] args)
+    {
+	
+	//CADASTRO DE TIPOS DE PRODUTO:
+	
+	//CADASTRO DE PRODUTO:
+	
+	//CADASTRO DE PRODUTOR:
+	
+	//CADASTRO DE PRODUTOR-PRODUTO:
+	
+	//CADASTRO DE CERTIFICADORA:
+	
+	//CADASTRO DE GRUPO:
+	
+	//CADASTRO DE CIDADE:
+
+
+    }
+}
