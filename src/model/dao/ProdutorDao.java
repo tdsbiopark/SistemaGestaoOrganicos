53fd626199ae84fd4eb3a7469051f54cb3d9486f
Andre Lucyo <andre.lucyo2@gmail.com>
2020-08-13 01:11:39 -0300

    criado metodos update, deleteById, iniciado metodos de pesquisa.

--- src/model/dao/ProdutorDao.java
+++ src/model/dao/ProdutorDao.java
@@ -5,6 +5,7 @@
  */
 package model.dao;
 
+import db.DbException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -12,6 +13,9 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
 import model.bd.DB;
+import model.entidades.Certificadora;
+import model.entidades.Cidade;
+import model.entidades.Grupo;
 import model.entidades.Produtor;
 
 /**
@@ -29,6 +33,11 @@ public class ProdutorDao
 	this.conexao = conexao;
     }
 
+    /**
+     * Faz inserte de um novo registro
+     *
+     * @param obj
+     */
     public void insert(Produtor obj)
     {
 	PreparedStatement st = null;
@@ -79,13 +88,14 @@ public class ProdutorDao
 	    }
 	    else
 	    {
-		// Se nao afetou nenhuma linha:
-		System.out.println("Nao afetou nenhuma linha!");
+		// Se nao afetou nenhuma linha, lança exceção:
+		throw new DbException("Erro inesperado! Nenhuma registro alterado!");
 	    }
 	}
 	catch (SQLException e)
 	{
-	    System.out.println("Erro ao inserir objeto!\nErro:" + e.getMessage());
+	    // Se der erro de SQL, lança SQLException:
+	    throw new DbException("Erro ao inserir novo registro!\n" + e.getMessage());
 	}
 	finally
 	{
@@ -94,6 +104,11 @@ public class ProdutorDao
 
     }
 
+    /**
+     * Faz alterações em um registro
+     *
+     * @param obj
+     */
     public void update(Produtor obj)
     {
 	PreparedStatement st = null;
@@ -136,14 +151,15 @@ public class ProdutorDao
 	    }
 	    else
 	    {
-		// Se nao afetou nenhuma linha:
-		System.out.println("Nada foi alterado!");
+		// Se nao afetou nenhuma linha, lança exceção:
+		throw new DbException("Erro inesperado! Nenhuma registro alterado!");
 	    }
 
 	}
 	catch (SQLException e)
 	{
-	    System.out.println("Erro ao alterar objeto!\nErro:" + e.getMessage());
+	    // Se der erro de SQL, lança SQLException:
+	    throw new DbException("Erro ao atualizar o registro!\n" + e.getMessage());
 	}
 	finally
 	{
@@ -151,14 +167,160 @@ public class ProdutorDao
 	}
     }
 
-    public void deleteById(int id)
+    public void deleteById(Integer id)
     {
+	PreparedStatement st = null;
+	try
+	{
+	    st = conexao.prepareStatement("DELETE FROM produtor WHERE Id = ?");
+
+	    st.setInt(1, id);
+
+	    // Pega o numero de linhas afetadas:
+	    int rowsAffected = st.executeUpdate();
+
+	    // Se não for afetado nenhuma linha significa que o Id nao existe
+	    if (rowsAffected == 0)
+	    {
+		// Se nao afetou nenhuma linha, lança exceção:
+		throw new SQLException("Id informado nao encontrado!  nada foi alterado!");
+	    }
+
+	}
+	catch (SQLException e)
+	{
+	    // Se der erro de SQL, lança SQLException:
+	    throw new DbException("Erro ao deletar o registro!\n" + e.getMessage());
+	}
+	finally
+	{
+	    DB.closeStatement(st);
+	}
+    }
+
+    public List<Produtor> findAll(Integer id)
+    {
+	return null;
+    }
+
+    //PAREI AQUI!!
+    public Produtor findById(Integer id)
+    {
+
+	PreparedStatement st = null;
+	ResultSet rs = null;
+
+	try
+	{
+
+	    // Inicia o prepareStatement:
+	    st = conexao.prepareStatement("SELECT prod.*, ce.*, cd.*, gr.* "
+		    + "FROM produtor as prod "
+		    + "INNER JOIN certificadora AS ce "
+		    + "INNER JOIN grupo as gr "
+		    + "INNER JOIN cidade as cd "
+		    + "ON "
+		    + "prod.certificadora_id = ce.id "
+		    + "AND"
+		    + " prod.grupo_id = gr.id "
+		    + "AND"
+		    + " prod.cidade_id = cd.id "
+		    + "WHERE prod.id = ?");
+
+	    // Passa o id para consulta
+	    st.setInt(1, id);
+
+	    // Recebe o resultado:
+	    rs = st.executeQuery();
+
+	    // testa se retornou algum resultado:
+	    if (rs.next())
+	    {
+
+		// Tranforma os dados da tabela em lista de objetos
+		Certificadora cert = instanciaCertificadora(rs);
+		Grupo grup = instanciaGrupo(rs);
+		Cidade cid = instanciaCidade(rs);
+
+		// VENDEDOR:
+		Produtor obj = instanciaProdutor(rs, cert, grup, cid);
+
+		return obj;
+
+	    }
+
+	    // se nao vem resultado retorna null
+	    return null;
+
+	}
+	catch (SQLException e)// Captura e exceção de SQL
+	{
+
+	    throw new DbException(e.getMessage());
+
+	}
+	finally
+	{
+
+	    DB.closeStatement(st);
+	    DB.closeResultSet(rs);
+
+	}
+    }
+
+    //===============================================================================
+    // METODOS AUXILIARES PARA INSTANCIAR OBJETOS
+    //===============================================================================
+    
+    /**
+     * Passar os dados do resultset para o objeto, propaga a exceção caso der
+     * erro
+     *
+     */
+    private Produtor instanciaProdutor(ResultSet rs, Certificadora cert, Grupo grupo, Cidade cidade) throws SQLException
+    {
+	// VENDEDOR
+	Produtor obj = new Produtor();
+	obj.setCertificadora(cert);
+	obj.setGrupo(grupo);
+	obj.setCidade(cidade);
+	// Nome das colunas da consulta:
+	obj.setId(rs.getInt("id"));
+	obj.setNome(rs.getString("nome"));
+	obj.setNome_propriedade(rs.getString("nome_propriedade"));
+	obj.setCpf_cnpj(rs.getString("cpf_cnpj"));
+	obj.setNumero(rs.getInt("numero"));
+	obj.setTelefone(rs.getString("telefone"));
+	obj.setEndereco(rs.getString("endereco"));
+	obj.setBairro(rs.getString("bairro"));
+	obj.setLatitude(rs.getDouble("latitude"));
+	obj.setLongitude(rs.getDouble("longitude"));
+	obj.setCertificacao_organicos(rs.getString("certificacao_organicos"));
+	obj.setVenda_consumidorfinal(rs.getBoolean("venda_consumidorfinal"));
+	obj.setRegistro_ativo(rs.getBoolean("registro_ativo"));
+	return obj;
 
     }
 
-    public List<Produtor> findAll()
+    private Certificadora instanciaCertificadora(ResultSet rs)
     {
-	return null;
+	Certificadora obj = new Certificadora();
+
+	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private Grupo instanciaGrupo(ResultSet rs)
+    {
+	Grupo obj = new Grupo();
+
+	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private Cidade instanciaCidade(ResultSet rs)
+    {
+	Cidade obj = new Cidade();
+
+	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
